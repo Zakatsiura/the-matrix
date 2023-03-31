4c2ba79f65aed33a5f658260e21d43a13e4549d3
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-03-31 11:14:12 +0300

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.font = "bold 16px monospace";
